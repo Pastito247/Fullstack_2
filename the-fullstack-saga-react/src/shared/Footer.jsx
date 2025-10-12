ca908cbe340e9254b6ed6304312cc34d63b6b384
Pasto247 <117881893+Pastito247@users.noreply.github.com>
2025-10-12 17:54:46 -0300

    muelte
    Diseño anterior no funciono asi que se empezada otro desde 0 pq estoy al borde del colapso mental :b

--- the-fullstack-saga-react/src/shared/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-4 text-center small opacity-75">
-      © 2025 The Fullstack Saga. Todos los derechos reservados.
-    </footer>
-  );
-}
