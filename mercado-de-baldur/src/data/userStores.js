bdc7f8c416ad3551bd6dd891cd3c7035ed2dffd5
Pasto247 <117881893+Pastito247@users.noreply.github.com>
2025-10-16 14:02:55 -0300

    reinicio

--- mercado-de-baldur/src/data/userStores.js
+++
@@ -1,5 +0,0 @@
-export const tiendasUsuarios =
-  JSON.parse(localStorage.getItem("tiendasUsuarios")) || [];
-
-export const guardarTiendasUsuarios = (lista) =>
-  localStorage.setItem("tiendasUsuarios", JSON.stringify(lista));
