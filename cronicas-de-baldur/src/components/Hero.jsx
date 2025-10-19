522d6538b4334fa437aac6a3b47366adf80838a1
Pasto247 <117881893+Pastito247@users.noreply.github.com>
2025-10-19 14:49:42 -0300

    Cambio
    Se cambia el tema porque no encontre api que tuviera todos los datos

--- cronicas-de-baldur/src/components/Hero.jsx
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-  return (
-    <div className="text-center py-5 bg-dark border border-warning rounded">
-      <h1 className="display-4">🧙‍♂️ Crónicas de Baldur</h1>
-      <p>Tu portal al multiverso de aventuras, magia y leyendas.</p>
-    </div>
-  );
-}
